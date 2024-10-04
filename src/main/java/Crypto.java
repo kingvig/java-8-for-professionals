6547a8fd669b90ca563087e7d06ded253c618eee
DJOUMESSI Vigny Arthur <VDI15@dsone.3ds.com>
2024-10-04 08:40:42 +0200

    small program to apply cesar cryptographic algorithm

--- src/main/java/Crypto.java
+++ src/main/java/Crypto.java
@@ -23,6 +23,30 @@ public class Crypto {
         //1- supprimer les elements superflu dans la chaine
         //2- fonction de chiffrement
 
+        aCoder=s.toLowerCase();
+        StringBuilder cleanedString = new StringBuilder();
+        StringBuilder codeddString = new StringBuilder();
+
+        for(int i = 0; i<aCoder.length(); i++){
+            char c = aCoder.charAt(i);
+            if(Character.isLetter(c) || c == ' '){
+                cleanedString.append(c);
+            }
+        }
+        aCoder= String.valueOf(cleanedString);
+
+
+        for(int i = 0; i<aCoder.length(); i++){
+            char c = aCoder.charAt(i);
+            if(Character.isLetter(c)) {
+                codeddString.append(ALPHABET.charAt((ALPHABET.indexOf(c) + DECALAGE)%26));
+            }else {
+                codeddString.append(' ');
+            }
+        }
+        chaineCodee= String.valueOf(codeddString);
+
+
         /*******************************************
          * Ne rien modifier apres cette ligne.
          *******************************************/
